9766b00fc914c12dc566771dde545469339eb3c9
Rajeev Sakhuja <sakhujar@amazon.com>
2021-08-11 13:25:37 -0400

    ok

--- bin/install/setup-bastion.sh
+++ bin/install/setup-bastion.sh
@@ -6,16 +6,22 @@
 
 # Setup the environment
 if [ -z "$1" ]; then
+    echo "Must set default region"
+    echo "Usage ./setup-bastion.sh  <<AWS Region>>  <<Cluster ID>>"
+    exit 1
+else
     export AWS_DEFAULT_REGION="$1"
 fi
 
 
 if [ -z "$2" ]; then
+    echo "Will use the default cluster ID !!"
+else
     export PG_CLUSTER_ID="$2"
 fi
 
 
-echo "Settings AWS_DEFAULT_REGION=$AWS_DEFAULT_REGION"
+echo "Setting  AWS_DEFAULT_REGION=$AWS_DEFAULT_REGION"
 echo "Setting  PG_CLUSTER_ID=$PG_CLUSTER_ID"
 
 
