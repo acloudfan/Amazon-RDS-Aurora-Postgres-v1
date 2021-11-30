c6156e0a0f3349aec025b8f6a14a9e33d0aec508
acloudfan <raj@acloudfan.com>
2021-11-30 09:00:43 -0500

    security iam

--- security/iam/cleanup.sh
+++ security/iam/cleanup.sh
@@ -9,6 +9,12 @@ IAM_DBUSER_ROLE=rdsa-iam-dbuser-role
 IAM_DBUSER_POLICY_ARN=$(aws iam list-policies --query "Policies[?PolicyName=='$IAM_DBUSER_POLICY'].Arn" --output text)
 IAM_DBUSER_ROLE_ARN=$(aws iam list-roles --query "Roles[?RoleName=='$IAM_DBUSER_ROLE'].Arn" --output text)
 
+# detach the policy from role
+aws iam detach-role-policy --role-name $IAM_DBUSER_ROLE --policy-arn $IAM_DBUSER_POLICY_ARN
+
+# delete the role
 aws iam delete-role --role-name $IAM_DBUSER_ROLE
+
+# delete the policy
 aws iam delete-policy --policy-arn $IAM_DBUSER_POLICY_ARN
 
