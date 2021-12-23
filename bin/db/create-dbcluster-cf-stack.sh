c0f3d18cf28895e072158cb48704ec1f6ae37ca5
acloudfan <raj@acloudfan.com>
2021-12-23 09:35:50 -0500

    bastion utility script

---
+++ bin/db/create-dbcluster-cf-stack.sh
@@ -0,0 +1,36 @@
+#!/bin/bash
+# Creates the RDSA PG Cluster 
+# DEPENDENCY on CloudFormation Stack: rdsa-vpc
+
+RDSA_VPC_CF_STACK_NAME="rdsa-vpc"
+RDSA_SG_CF_STACK_NAME="rdsa-security-group"
+RDSA_CLUSTER_CF_STACK_NAME="rdsa-postgresql"
+TEMPLATE_LOCATION=file://home/ec2-user/cloudformation/rdsa-postgresql.yml \
+
+# Get the comma seperated list of subnets
+VPC_ID=$(aws cloudformation  describe-stacks --stack-name rdsa-vpc --query 'Stacks[0].Outputs[?ExportName==`us-east-2-rdsa-vpc-MainVPC`].OutputValue | [0]')
+PRIVATE_SUBNETS=$(OutputValue rdsa-vpc --query 'Stacks[0].Outputs[?ExportName==`rdsa-vpc-SubnetsPrivate`].OutputValue | [0]')
+RDSA_INTERNAL_SG=$(aws cloudformation  describe-stacks --stack-name rdsa-security-group --query 'Stacks[0].Outputs[?OutputKey==`DBSecurityGroupCluster`].OutputValue | [0]')
+
+if [ -z "VPC_ID" ]; then
+    echo "Looks like the stack rdsa-vpc is not there!!"
+    echo "Please check - Aborting :("
+    exit
+fi
+
+# Show the info used for 
+echo "VPC ID = $VPC_ID"
+echo "Private Subnet List = $PRIVATE_SUBNETS"
+echo "Security Group = $RDSA_INTERNAL_SG"
+
+
+aws cloudformation create-stack    \  
+--stack-name $RDSA_CLUSTER_CF_STACK_NAME   \ 
+--template-body $TEMPLATE_LOCATION  \
+--parameters ParameterKey=TemplateName,ParameterValue="$RDSA_CLUSTER_CF_STACK_NAME"  \
+ParameterKey=TemplateName,ParameterValue=RdsAuroraVPC,ParameterValue="$VPC_ID"  \
+ParameterKey=PrivateSubnets,ParameterValue="$PRIVATE_SUBNETS" \
+ParameterKey=DBMasterUsername,ParameterValue="masteruser" \
+ParameterKey=DBMasterUserPassword,ParameterValue="masteruserpw" \
+ParameterKey=VPCSecurityGroupCluster,ParameterValue="$RDSA_INTERNAL_SG" \
+--capabilities "CAPABILITY_NAMED_IAM"
---
+++ bin/db/delete-dbcluster-stack-cf-stack.sh
@@ -0,0 +1,24 @@
+#!/bin/bash
+# This scripts deletes an existing CloudFormation stack
+# If there is a DRIFT then CF may fail to delete the cluster
+
+RDSA_CLUSTER_CF_STACK_NAME="rdsa-postgresql"
+
+# Confirm from user
+while true; do
+    read -p "Are you sure, you want to DELETE the Aurora clust [$RDSA_CLUSTER_CF_STACK_NAME]?" yn
+    case $yn in
+        [Nn]* ) exit;;
+        * ) echo "Please answer yes/y or no/n.";;
+    esac
+done
+
+echo "Be aware that sometimes, the stack deletion fails. It is your responsibility to ensure that the cluster is successfully deleted !!!"
+
+# Delete the stack
+aws  delete-stack --stack-name $RDSA_CLUSTER_CF_STACK_NAME="rdsa-postgresql"
+
+# Continuously check the delete status
+aws  stack-delete-complete  --stack-name  $RDSA_CLUSTER_CF_STACK_NAME="rdsa-postgresql"
+
+echo "Done."
