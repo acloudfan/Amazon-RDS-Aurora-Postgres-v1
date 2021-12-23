4f9b71a9461d1af122d8037ad3de38036b9aa886
acloudfan <raj@acloudfan.com>
2021-12-23 08:23:37 -0500

    bastion utility script

--- bin/dbcluster.sh
+++ bin/dbcluster.sh
@@ -7,15 +7,21 @@ DB_CLUSTER_ID="rdsa-postgresql-cluster"
 # By deault script will print the cluster status
 if [ -z "$1" ]; then
     ACTION="status"
+elif [[ "stop" == "$ACTION" ]]; then
+     echo  "REMEMBER : RDS Database clusters start automatically after 7 days !!!"
 fi
 
-if [ "status" = "$ACTION" ]; then
+if [[ "status" == "$ACTION" ]]; then
      STATUS=$(aws rds describe-db-clusters  --db-cluster-identifier "$DB_CLUSTER_ID"   --query 'DBClusters[0].Status')
      echo "DB Cluster [$DB_CLUSTER_ID]: $STATUS"
-elif [ "$ACTION"=="stop" ]; then
+elif [[ "$ACTION" == "stop" ]]; then
      aws rds stop-db-cluster --db-cluster-identifier $DB_CLUSTER_ID
-elif [ "$ACTION"=="start" ]; then
+     STATUS=$(aws rds describe-db-clusters  --db-cluster-identifier "$DB_CLUSTER_ID"   --query 'DBClusters[0].Status')
+     echo "DB Cluster [$DB_CLUSTER_ID]: $STATUS"
+elif [[ "$ACTION" == "start" ]]; then
      aws rds start-db-cluster --db-cluster-identifier $DB_CLUSTER_ID
+     STATUS=$(aws rds describe-db-clusters  --db-cluster-identifier "$DB_CLUSTER_ID"   --query 'DBClusters[0].Status')
+     echo "DB Cluster [$DB_CLUSTER_ID]: $STATUS"
 else
     echo "Usage: dbluster [default = status | start | stop]"
 fi
