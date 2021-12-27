c0fb5d09cd84ba9d6f23b8111dd8bcf35aeb9517
acloudfan <raj@acloudfan.com>
2021-12-27 06:54:43 -0500

    replica CF scripts

---
+++ bin/db/create-replica.sh
@@ -0,0 +1,55 @@
+#!/bin/bash
+# Creates a Named Replica
+# Usage: ./bin/db/create-replica.sh  <<node name>>
+#        ./bin/db/create-replica.sh  node-02
+
+if [ -z "$1" ]; then
+    echo "Usage: ./bin/db/create-replica.sh  <<node name>>  [Instance type   db.t3.medium=default] [Engine version  13.4=default]"
+    echo "       ./bin/db/create-replica.sh  node-02"
+    exit
+fi
+
+SLEEP_TIME=5s
+RDSA_CLUSTER_CF_STACK_NAME="rdsa-postgresql"
+RDSA_REPLICA_CF_STACK_NAME="rdsa-postgresql-$1"
+
+TEMPLATE_LOCATION=file:///home/ec2-user/cloudformation/postgres-cluster-replica.yml \
+
+# CF Parameters
+RDSA_DB_CLUSTER="rdsa-postgresql"
+DB_INSTANCE_TYPE="db.t3.medium"
+if [ -z "$2" ]; then
+    echo "DB Instance Type=$DB_INSTANCE_TYPE"
+else
+    DB_INSTANCE_TYPE="$2"
+    echo "DB Instance Type=$DB_INSTANCE_TYPE"
+fi
+
+DB_ENGINE_VERSION="13.4"
+if [ -z "$2" ]; then
+    echo "DB Engine Version=$DB_ENGINE_VERSION"
+else
+    DB_ENGINE_VERSION="$3"
+    echo "DB Instance Type=$DB_ENGINE_VERSION"
+fi
+
+# Create the stack
+aws cloudformation create-stack \
+--stack-name "$RDSA_REPLICA_CF_STACK_NAME" --template-body "$TEMPLATE_LOCATION"  \
+--parameters ParameterKey=TemplateName,ParameterValue="$RDSA_CLUSTER_CF_STACK_NAME"  \
+ParameterKey=DBInstanceClass,ParameterValue="$DB_INSTANCE_TYPE"  \
+ParameterKey=DBEngineVersion,ParameterValue="$DB_ENGINE_VERSION"  \
+ParameterKey=DBCluster,ParameterValue="$RDSA_DB_CLUSTER"  
+
+# Wait for the stack to be created
+while [ $? == 0 ]; do
+    sleep $SLEEP_TIME
+    CF_STACK_STATUS=$(aws  cloudformation describe-stacks --stack-name $RDSA_REPLICA_CF_STACK_NAME --output text --query 'Stacks[0].StackStatus')
+    if [[ $CF_STACK_STATUS == "CREATE_IN_PROGRESS" ]]; then
+        echo -n "."
+    else
+        break
+    fi
+done;
+echo "."
+echo "Done."
---
+++ bin/db/delete-replica.sh
@@ -0,0 +1,44 @@
+#!/bin/bash
+#Deletes the specified cluster
+# Usage: ./bin/db/delete-replica.sh  <<node name>>
+#        ./bin/db/delete-replica.sh  node-02
+
+if [ -z "$1" ]; then
+    echo "Usage: ./bin/db/create-replica.sh  <<node name>>  [Instance type   db.t3.medium=default] [Engine version  13.4=default]"
+    echo "       ./bin/db/create-replica.sh  node-02"
+    exit
+fi
+
+SLEEP_TIME=5s
+RDSA_REPLICA_CF_STACK_NAME="rdsa-postgresql-$1"
+
+# Confirm from user
+while true; do
+    read -p "Are you sure, you want to DELETE the replica stack [$RDSA_REPLICA_CF_STACK_NAME] ?" yn
+    case $yn in
+        [Nn]* ) exit;;
+        [Yy]* ) break;;
+        * ) echo "Please answer yes/y or no/n.";;
+    esac
+done
+
+echo "Be aware that sometimes, the stack deletion fails !!"
+echo "NOTE: It is your responsibility to ensure that the cluster is successfully deleted."
+
+
+# Delete the stack
+aws cloudformation delete-stack --stack-name "$RDSA_REPLICA_CF_STACK_NAME"
+
+# Continuously check the delete status
+while [ $? == 0 ]; do
+    sleep $SLEEP_TIME
+    STATUS=$(aws  cloudformation describe-stacks --output text --stack-name "$RDSA_REPLICA_CF_STACK_NAME" --query 'Stacks[0].StackStatus')
+    if [[ "$STATUS" == "DELETE_IN_PROGRESS" ]]; then
+        echo -n "."
+    else
+        break
+    fi
+done;
+echo "."
+
+echo "Done."
