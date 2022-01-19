5afa7a83333e0a1e712e0e5f46dfa445988c082c
acloudfan <raj@acloudfan.com>
2022-01-19 08:54:04 -0500

    global

--- bin/db/delete-security-group-internal.sh
+++ bin/db/delete-security-group-internal.sh
@@ -6,16 +6,4 @@ RDSA_SG_CF_STACK_NAME="rdsa-security-group"
 # Delete the stack
 aws cloudformation delete-stack --stack-name "$RDSA_SG_CF_STACK_NAME"
 
-# Continuously check the delete status
-while [ $? == 0 ]; do
-    sleep 5
-    STATUS=$(aws  cloudformation describe-stacks --output text --stack-name "$RDSA_SG_CF_STACK_NAME" --query 'Stacks[0].StackStatus')
-    if [[ "$STATUS" == "DELETE_IN_PROGRESS" ]]; then
-        echo -n "."
-    else
-        break
-    fi
-done;
-echo "."
-
 echo "Done."
