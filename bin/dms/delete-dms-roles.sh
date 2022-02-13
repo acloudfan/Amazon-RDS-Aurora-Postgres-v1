85f480c39e1b93c0d30c92a723f4ebb8b8d410ec
acloudfan <raj@acloudfan.com>
2022-02-13 13:06:23 -0500

    dms

--- bin/dms/delete-dms-roles.sh
+++ bin/dms/delete-dms-roles.sh
@@ -8,6 +8,9 @@ DMS_VPC_ROLE_ARN=$(aws iam list-roles --query "Roles[?RoleName=='$DMS_VPC_ROLE']
 if  [ "$DMS_VPC_ROLE_ARN" == "" ]; then
     echo "Role Not Found - No action taken."
 else 
+    DMS_VPC_MANAGEMENT_ROLE=$(aws iam list-policies --query "Policies[?PolicyName=='$DMS_VPC_MANAGEMENT_ROLE'].Arn" --output text)
+    echo "Detaching the  DMS_VPC_ROLE"
+    aws iam detach-role-policy --role-name $DMS_VPC_ROLE --policy-arn $DMS_VPC_MANAGEMENT_ROLE
     aws iam delete-role --role-name $DMS_VPC_ROLE
     echo "Done."
 fi
