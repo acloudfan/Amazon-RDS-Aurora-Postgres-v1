c0727fa684f8ab029f06d81659f44e5ae1dbe18a
acloudfan <raj@acloudfan.com>
2022-02-13 13:21:34 -0500

    dms

--- bin/dms/create-dms-roles.sh
+++ bin/dms/create-dms-roles.sh
@@ -36,56 +36,53 @@ echo "DMS_VPC_ROLE Arn=$DMS_VPC_ROLE_ARN"
 
 # Attach the role to policy IAM_COPY_TASK_POLICY
 echo "Attaching the IAM policy to IAM role"
-# DMS_VPC_MANAGEMENT_ROLE_ARN=$(aws iam list-policies --query "Policies[?PolicyName=='$DMS_VPC_MANAGEMENT_ROLE'].Arn" --output text)
-# echo "DMS_VPC_MANAGEMENT_ROLE_ARN=$DMS_VPC_MANAGEMENT_ROLE_ARN"
-# aws iam attach-role-policy   --role-name $DMS_VPC_ROLE  --policy-arn  $DMS_VPC_MANAGEMENT_ROLE_ARN
-
 aws iam attach-role-policy   --role-name $DMS_VPC_ROLE   --role-name dms-vpc-role --policy-arn arn:aws:iam::aws:policy/service-role/$DMS_VPC_MANAGEMENT_ROLE
 else
     echo "dms_vpc_role role exists - no action taken !!"
 fi
 
-#---------------------------------
+#-----------------------------------
 
 
 
 # #---------------------------------
-# DMS_CW_ROLE=dms-cloudwatch-logs-role
+DMS_CW_ROLE=dms-cloudwatch-logs-role
+DMS_CW_MANAGEMENT_ROLE=AmazonDMSCloudWatchLogsRole
 
-# # Check if the role exist
-# DMS_CW_ROLE_ARN=$(aws iam list-roles --query "Roles[?RoleName=='$DMS_CW_ROLE'].Arn" --output text)
+# Check if the role exist
+DMS_CW_ROLE_ARN=$(aws iam list-roles --query "Roles[?RoleName=='$DMS_CW_ROLE'].Arn" --output text)
 
-# if  [ "$DMS_CW_ROLE_ARN" == "" ]; then
-# read -r -d ''  ASSUME_ROLE_POLICY << EOL {  
-#     "Version": "2012-10-17",
-#     "Statement": [
-#     {
-#       "Sid": "",
-#       "Effect": "Allow",
-#       "Principal": {
+if  [ "$DMS_CW_ROLE_ARN" == "" ]; then
+read -r -d ''  ASSUME_ROLE_POLICY << EOL {  
+    "Version": "2012-10-17",
+    "Statement": [
+    {
+      "Sid": "",
+      "Effect": "Allow",
+      "Principal": {
        
-#     "Service": "dms.amazonaws.com"
-#       },
-#       "Action": "sts:AssumeRole"
-#     }
-#   ]
-# }
-# EOL
-# # Write out the trust policy to temp file
-# echo "$ASSUME_ROLE_POLICY" > /tmp/assume-policy.json
+    "Service": "dms.amazonaws.com"
+      },
+      "Action": "sts:AssumeRole"
+    }
+  ]
+}
+EOL
+# Write out the trust policy to temp file
+echo "$ASSUME_ROLE_POLICY" > /tmp/assume-policy.json
 
-# # Create the role with the trust policy
-# aws iam create-role --role-name $DMS_CW_ROLE --assume-role-policy-document  file:///tmp/assume-policy.json  > /dev/null
-# DMS_CW_ROLE_ARN=$(aws iam list-roles --query "Roles[?RoleName=='$DMS_CW_ROLE'].Arn" --output text)
+# Create the role with the trust policy
+aws iam create-role --role-name $DMS_CW_ROLE --assume-role-policy-document  file:///tmp/assume-policy.json  > /dev/null
+DMS_CW_ROLE_ARN=$(aws iam list-roles --query "Roles[?RoleName=='$DMS_CW_ROLE'].Arn" --output text)
 
-# echo "DMS_CW_ROLE Arn=$DMS_CW_ROLE_ARN"
+echo "DMS_CW_ROLE Arn=$DMS_CW_ROLE_ARN"
 
-# # Attach the role to policy IAM_COPY_TASK_POLICY
-# echo "Attaching the IAM policy to IAM role"
-# aws iam attach-role-policy   --role-name $DMS_VPC_ROLE  --policy-arn  $DMS_VPC_ROLE_ARN
-# else
-#     echo "dms_vpc_role role exists - no action taken !!"
-# fi
+# Attach the role to policy IAM_COPY_TASK_POLICY
+echo "Attaching the IAM policy to IAM role"
+aws iam attach-role-policy   --role-name $DMS_CW_ROLE   --role-name dms-vpc-role --policy-arn arn:aws:iam::aws:policy/service-role/$DMS_CW_MANAGEMENT_ROLE
+else
+    echo "$DMS_CW_ROLE role exists - no action taken !!"
+fi
 
 # #-------------------------
 
