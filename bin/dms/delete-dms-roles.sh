3bd560e61db0fd602ead737a0d81dc8a30166b19
acloudfan <raj@acloudfan.com>
2022-02-13 13:31:06 -0500

    dms

--- bin/dms/delete-dms-roles.sh
+++ bin/dms/delete-dms-roles.sh
@@ -4,15 +4,15 @@
 
 #----dms_vpc_role----
 # Check if the role exist
-DMS_VPC_ROLE=dms_vpc_role
+DMS_VPC_ROLE=dms-vpc-role
 DMS_VPC_ROLE_ARN=$(aws iam list-roles --query "Roles[?RoleName=='$DMS_VPC_ROLE'].Arn" --output text)
 DMS_VPC_MANAGEMENT_ROLE=AmazonDMSVPCManagementRole
 
 if  [ "$DMS_VPC_ROLE_ARN" == "" ]; then
-    echo "Role Not Found - No action taken."
+    echo "Role $DMS_VPC_ROLE Not Found - No action taken."
 else 
     DMS_VPC_MANAGEMENT_ROLE_ARN=$(aws iam list-policies --query "Policies[?PolicyName=='$DMS_VPC_MANAGEMENT_ROLE'].Arn" --output text)
-    echo "Detaching the  DMS_VPC_ROLE"
+    echo "Detaching the  $DMS_VPC_MANAGEMENT_ROLE"
     aws iam detach-role-policy --role-name $DMS_VPC_ROLE --policy-arn $DMS_VPC_MANAGEMENT_ROLE_ARN
     echo "Deleting the $DMS_VPC_ROLE"
     aws iam delete-role --role-name $DMS_VPC_ROLE
@@ -20,16 +20,16 @@ else
 fi
 
 
-#----dms_vpc_role----
+#----dms-cloudwatch-logs-role----
 # Check if the role exist
 DMS_CW_ROLE=dms-cloudwatch-logs-role
 DMS_CW_MANAGEMENT_ROLE=AmazonDMSCloudWatchLogsRole
 
-DMS_CW_ROLE_ARN=$(aws iam list-roles --query "Roles[?RoleName=='$DMS_VPC_ROLE'].Arn" --output text)
+DMS_CW_ROLE_ARN=$(aws iam list-roles --query "Roles[?RoleName=='$DMS_CW_ROLE'].Arn" --output text)
 
 
 if  [ "$DMS_CW_ROLE_ARN" == "" ]; then
-    echo "Role Not Found - No action taken."
+    echo "Role $DMS_CW_ROLE Not Found - No action taken."
 else 
     DMS_CW_MANAGEMENT_ROLE_ARN=$(aws iam list-policies --query "Policies[?PolicyName=='$DMS_CW_MANAGEMENT_ROLE'].Arn" --output text)
     echo "Detaching the  DMS_VPC_ROLE"
