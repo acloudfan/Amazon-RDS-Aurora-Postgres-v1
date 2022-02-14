ba10a22ea6a566015d571995be501cbbbfb9bcb7
acloudfan <raj@acloudfan.com>
2022-02-14 08:49:34 -0500

    dms

--- bin/dms/delete-replication-task.sh
+++ bin/dms/delete-replication-task.sh
@@ -19,26 +19,22 @@ fi
 # Stop the replication task
 # https://docs.aws.amazon.com/cli/latest/reference/dms/stop-replication-task.html
 STATUS=$(aws  dms describe-replication-tasks  --output text --query "ReplicationTasks[?ReplicationTaskIdentifier=='${REPL_TASK_IDENTIFIER}'].Status | [0]")
-if [[ "$STATUS" != "stopped" ]]; then
+if [[ "$STATUS" == "running" ]]; then
+    echo "Attempting to stop: $REPL_TASK_ARN"
     aws dms stop-replication-task --replication-task-arn $REPL_TASK_ARN
+    # Continuously check the  status
+    while [ $? == 0 ]; do
+        sleep $SLEEP_WAIT
+        STATUS=$(aws  dms describe-replication-tasks  --output text --query "ReplicationTasks[?ReplicationTaskIdentifier=='${REPL_TASK_IDENTIFIER}'].Status | [0]")
+        if [[ "$STATUS" == "stopped" ]]; then
+            break
+        else
+            echo -n "."
+        fi
+    done;
 else
-    echo "Task already STOPPED"
+    echo "Task already STOPPED or READY"
 fi
-
-
-
-echo "Attempting to stop: $REPL_TASK_ARN"
-# Continuously check the  status
-while [ $? == 0 ]; do
-    sleep $SLEEP_WAIT
-    STATUS=$(aws  dms describe-replication-tasks  --output text --query "ReplicationTasks[?ReplicationTaskIdentifier=='${REPL_TASK_IDENTIFIER}'].Status | [0]")
-    if [[ "$STATUS" == "stopped" ]]; then
-        break
-    else
-        echo -n "."
-    fi
-done;
-
 
 # Delete the replication task
 aws dms delete-replication-task \
